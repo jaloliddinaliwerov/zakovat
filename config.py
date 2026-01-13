6b4d7acff4883ba1ef47871837b39ceefee085fb
jaloliddinaliwerov <alisherovthe@gmail.com>
2026-01-13 23:09:32 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,15 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMINS = [6734269605]  # admin user_id lar
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # Railway variables
+
+ADMINS = [6734269605]  # 👉 o‘z Telegram ID’ing (int)
+
+DB_NAME = "database.db"
+
+# Majburiy azolik (hammasiga obuna bo‘lish shart)
+CHANNELS = [
+    "@Mavericks_Zakovat",
+    "@the7dvn"
+]
+
+
