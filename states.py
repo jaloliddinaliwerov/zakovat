287f2b9ff96a7f0060cd35adb8b68d80762230da
jaloliddinaliwerov <alisherovthe@gmail.com>
2026-01-13 23:37:43 +0500

    Update states.py

--- states.py
+++ states.py
@@ -1,5 +1,9 @@
-from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.state import State, StatesGroup
 
 class UserState(StatesGroup):
     team = State()
     test_code = State()
+
+class AdminState(StatesGroup):
+    create_test = State()
+    add_question = State()
