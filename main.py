4829c3d2e9a2bd2b24629de4d276b88c61de1d3e
jaloliddinaliwerov <alisherovthe@gmail.com>
2026-01-13 23:40:37 +0500

    Refactor main.py for cleaner initialization

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-
 from config import BOT_TOKEN
-from db import init_db
-from user import user_router
 from admin import admin_router
+from user import user_router
+from db import init_db
 
 async def main():
     await init_db()
-
     bot = Bot(BOT_TOKEN)
-    storage = MemoryStorage()
-    dp = Dispatcher(storage=storage)
-
-    # MUHIM: avval admin, keyin user
+    dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(admin_router)
     dp.include_router(user_router)
-
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
