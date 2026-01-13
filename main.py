d9259e5f39f1256bdbdd2f2b957d22f251bf3585
jaloliddinaliwerov <alisherovthe@gmail.com>
2026-01-13 23:20:09 +0500

    Refactor main.py to include admin router

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-import asyncio
 from aiogram import Bot, Dispatcher
+import asyncio
 
 from config import BOT_TOKEN
 from db import init_db
-from admin import admin_router
 from user import user_router
+from admin import admin_router   # agar admin.py bo‘lsa
 
 async def main():
     await init_db()
     bot = Bot(BOT_TOKEN)
     dp = Dispatcher()
+
     dp.include_router(admin_router)
     dp.include_router(user_router)
+
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
