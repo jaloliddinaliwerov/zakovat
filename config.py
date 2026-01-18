45866ee0ff640af0ecfc35cafb98ba73e2ce79f0
jaloliddinaliwerov <alisherovthe@gmail.com>
2026-01-18 11:00:39 +0500

    Refactor config.py by removing unused variables
    Removed redundant admin ID list and database name.

--- config.py
+++ config.py
@@ -1,15 +1,7 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")  # Railway variables
-
-ADMINS = [6734269605,7658297748]  # 👉 o‘z Telegram ID’ing (int)
-
-DB_NAME = "database.db"
-
-# Majburiy azolik (hammasiga obuna bo‘lish shart)
-CHANNELS = [
-    "@Mavericks_Zakovat",
-    "@the7dvn"
-]
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # Railway Variables
+ADMIN_IDS = [6734269605,7658297748]  # o'zingni admin id'ingni yoz
+REQUIRED_CHANNELS = ["@Mavericks_Zakovat", "@the7dvn"]
 
 
