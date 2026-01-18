16cb914e8f74402584f8eacc20f848aa96a501a7
jaloliddinaliwerov <alisherovthe@gmail.com>
2026-01-18 11:15:08 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")  # Railway Variables
 ADMIN_IDS = [6734269605,7658297748]  # o'zingni admin id'ingni yoz
-REQUIRED_CHANNELS = ["@Mavericks_Zakovat", "@the7dvn"]
-
+REQUIRED_CHANNELS = [
+    "@Mavericks_Zakovat",
+    "@the7dvn"
+]
 
