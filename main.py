2375f84851eb69cbe09f896a9fa3dcb716bc3637
jaloliddinaliwerov <alisherovthe@gmail.com>
2026-01-18 11:59:41 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,16 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
-from aiogram.fsm.storage.memory import MemoryStorage
 
 from config import BOT_TOKEN
 from admin import admin_router
 from user import user_router
 
 async def main():
-    bot = Bot(
-        token=BOT_TOKEN,
-        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
-    )
-
-    dp = Dispatcher(storage=MemoryStorage())  # 🔴 MUHIM
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
 
     dp.include_router(admin_router)
     dp.include_router(user_router)
-
-    print("BOT ISHGA TUSHDI")
 
     await dp.start_polling(bot)
 
