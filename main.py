c5df43041f0902c33c7426097affd357c67f02de
jaloliddinaliwerov <alisherovthe@gmail.com>
2026-01-18 11:03:52 +0500

    Refactor main.py to simplify bot initialization
    Removed database initialization and changed dispatcher storage.

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.fsm.storage.memory import MemoryStorage
+
 from config import BOT_TOKEN
 from admin import admin_router
 from user import user_router
-from db import init_db
 
 async def main():
-    await init_db()
-    bot = Bot(BOT_TOKEN)
-    dp = Dispatcher(storage=MemoryStorage())
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+
     dp.include_router(admin_router)
     dp.include_router(user_router)
+
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
